ae540dc07c73bf715415663aa001ddc705db47b2
JosuéOrozco01 <josuecitomerida@gmail.com>
2025-10-20 22:25:29 -0600

    Fix(Zombie): my zombies now kill, and they move in different places for kill my personage

--- Assets/zombies.cs
+++ Assets/zombies.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class zombies : MonoBehaviour
@@ -12,6 +14,14 @@ public class zombies : MonoBehaviour
     public float umbralVelicidad;
     public GameObject prefabMuerto;
     public float magnitudVueloCabeza = 300f;
+    enum tipoComportamientoZombie { pasivo, persecucion, ataque }
+    tipoComportamientoZombie comportamiento = tipoComportamientoZombie.pasivo;
+    float entradaZonaPersecucion = 40f;
+    float salidaZonaPersecucion = 80f;
+    float distaciaAtaque = 6f;
+    float distanciaShaggy;
+    public Transform Shaggy;
+    Animator anim;
 
     void Start()
     {
@@ -24,20 +34,73 @@ public class zombies : MonoBehaviour
 
         limiteCaminataIzq = centro - radioEscalado;
         limiteCaminataDer = centro + radioEscalado;
+
+        anim = transform.GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         if (rb.linearVelocity.magnitude < umbralVelicidad)
+            distanciaShaggy = Mathf.Abs(Shaggy.position.x - transform.position.x);
         {
-            // Movimiento del zombie
-            rb.linearVelocity = new Vector2(velCaminata * direccion, rb.linearVelocity.y);
+            switch (comportamiento)
+            {
+                case tipoComportamientoZombie.pasivo:
+                    {
+                        // Movimiento del zombie
+                        rb.linearVelocity = new Vector2(velCaminata * direccion, rb.linearVelocity.y);
 
-            // Cambio de dirección al alcanzar los límites
-            if (transform.position.x <= limiteCaminataIzq)
-                direccion = 1f;
-            else if (transform.position.x >= limiteCaminataDer)
-                direccion = -1f;
+                        // Cambio de dirección al alcanzar los límites
+                        if (transform.position.x <= limiteCaminataIzq)
+                            direccion = 1f;
+                        else if (transform.position.x >= limiteCaminataDer)
+                            direccion = -1f;
+
+                        anim.speed = 1f;
+
+                        //entrar en zona de persecucion 
+                        if (distanciaShaggy < entradaZonaPersecucion) comportamiento = tipoComportamientoZombie.persecucion;
+                    }
+                    break;
+
+                case tipoComportamientoZombie.persecucion:
+                    {
+                        // Movimiento del zombie (corriendo)
+                        rb.linearVelocity = new Vector2(velCaminata * 1.5f * direccion, rb.linearVelocity.y);
+
+                        // Cambio de dirección al alcanzar los límites
+                        if (Shaggy.position.x > transform.position.x)
+                            direccion = 1f;
+                        else if (Shaggy.position.x < transform.position.x)
+                            direccion = -1f;
+
+                        // velocidad del animator
+                        anim.speed = 1.5f;
+
+                        // volver a la zona pasiva  
+                        if (distanciaShaggy > salidaZonaPersecucion) comportamiento = tipoComportamientoZombie.pasivo;
+
+                        // entrar en zona de ataque
+                        if (distanciaShaggy < distaciaAtaque) comportamiento = tipoComportamientoZombie.ataque;
+                    }
+                    break;
+
+                case tipoComportamientoZombie.ataque:
+                    {
+                        // Cambio de dirección segun la posicion de shaggy
+                        if (Shaggy.position.x > transform.position.x)
+                            direccion = 1f;
+                        else if (Shaggy.position.x < transform.position.x)
+                            direccion = -1f;
+
+                        // velocidad del animator
+                        anim.speed = 1f;
+
+                        // entrar en zona de ataque
+                        if (distanciaShaggy > distaciaAtaque) comportamiento = tipoComportamientoZombie.persecucion;
+                    }
+                    break;
+            }
 
             // Ajusta la escala para girar el sprite
             Vector3 escala = transform.localScale;
@@ -46,12 +109,20 @@ public class zombies : MonoBehaviour
         }
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && comportamiento == tipoComportamientoZombie.ataque)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+    
     public void muere(Vector3 direccion)
     {
         GameObject instMuerto = Instantiate(prefabMuerto, transform.position, transform.rotation);
 
         instMuerto.transform.GetChild(0).GetComponent<Rigidbody2D>().AddForce(direccion * magnitudVueloCabeza, ForceMode2D.Impulse);
-        instMuerto.transform.GetChild(1).GetComponent<Rigidbody2D>().AddForce(direccion * magnitudVueloCabeza/2, ForceMode2D.Impulse);
+        instMuerto.transform.GetChild(1).GetComponent<Rigidbody2D>().AddForce(direccion * magnitudVueloCabeza / 2, ForceMode2D.Impulse);
         instMuerto.transform.GetChild(0).GetComponent<Rigidbody2D>().AddTorque(10f, ForceMode2D.Impulse);
         instMuerto.transform.GetChild(1).GetComponent<Rigidbody2D>().AddTorque(10f, ForceMode2D.Impulse);
         Destroy(gameObject);
